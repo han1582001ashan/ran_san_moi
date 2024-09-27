12d6fef7d8dcaa43c7bc4d3c3e0dfc2695b01a84
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-09-27 16:14:05 +0700

    nopbai

--- Program.cs
+++ Program.cs
@@ -3,31 +3,57 @@ namespace RanSanMoi
 {
     class Program
     {
-
+        public static SnakeControl snakeControl=new SnakeControl();
         static void Main(string[] args)
         {
 
 
-            Thread _game = new Thread(SnakeControl.ListenKey);
-bool EndGame= false;
+            Thread _game = new Thread(snakeControl.ListenKey);
+            bool EndGame = false;
+            int bestScore = SnakeControl.BestScore;
+
             _game.Start();
 
             do
             {
-                if(!SnakeControl.SNAKE_DIE){
-                Console.Clear();
-                SnakeControl.Drawboard();
-                SnakeControl.setUpBoard();
-                SnakeControl.MoveSnakeHead();
-                SnakeControl.EatFood();
-                SnakeControl.SpawnBody();
-                SnakeControl.PopUpfood();
-                SnakeControl.ShowPoint();
-                Thread.Sleep(SnakeControl.speed);
-                }else{
-                    Console.WriteLine("GAME OVER!!!");
-                    EndGame=true;
+                try
+                {
+                    if (!SnakeControl.SNAKE_DIE)
+                    {
+
+                        Console.Clear();
+                        snakeControl.Drawboard();
+                        snakeControl.setUpBoard();
+                        snakeControl.MoveSnakeHead();
+                        snakeControl.EatFood();
+                        snakeControl.SpawnBody();
+                        snakeControl.PopUpfood();
+                        snakeControl.ShowPoint();
+                        Thread.Sleep(SnakeControl.speed);
+                    }
+                    else
+                    {
+                        if (SnakeControl.score > bestScore)
+                        {
+                            SnakeControl.saveNewHighScore(SnakeControl.score.ToString());
+                            Console.WriteLine("GAME OVER!");
+                            Console.WriteLine("New best score: " + SnakeControl.score);
+                            EndGame = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("GAME OVER!!!");
+                            EndGame = true;
+                        }
+
+
+                    }
                 }
+                catch (Exception e)
+                {
+
+                }
+
 
 
             } while (!EndGame);
--- SnakeControl.cs
+++ SnakeControl.cs
@@ -1,16 +1,17 @@
- namespace RanSanMoi
+namespace RanSanMoi
 {
- public class SnakeControl
+    public class SnakeControl
     {
         public static Point food = new Point(8, 8);
         public static bool SNAKE_DIE = false;
         public static bool foodExist = false;
         public static int speed = 500;
         public static int row = 15;
+        public static int BestScore = Int32.Parse(getHighScore());
         public static int col = 40;
         public static string direction = "Right";
         public static int score;
-        public static string TITLE="RAN SAN MOI";
+        public static string TITLE = "RAN SAN MOI";
         public static Point[] body = new Point[2]
         {
             new Point(3,4),
@@ -19,180 +20,277 @@
         public static Point _head = new Point(10, 10);
         public static string[,] board = new string[row, col];
         // ve cac doi tuong tren ban do (bien, ran, moi)
-        public static void Drawboard()
+        public  void Drawboard()
+
         {
-            for (int i = 0; i < row; i++)
+            try
             {
-                for (int j = 0; j < col; j++)
+                for (int i = 0; i < row; i++)
                 {
-                    if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                    for (int j = 0; j < col; j++)
                     {
-                        board[i, j] = "#";
-                    }
-                    else if (i == _head.X && j == _head.Y)
-                    {
-                        board[i, j] = "o";
-                    }
-                    else
-                    {
-                        bool isBodyPart = false;
-                        for (int count = 0; count < body.Length; count++)
+                        if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
                         {
-                            if (i == body[count].X && j == body[count].Y)
-                            {
-                                board[i, j] = "+";
-                                isBodyPart = true;
-                                break;
-                            }
+                            board[i, j] = "#";
                         }
-                        if (!isBodyPart)
+                        else if (i == _head.X && j == _head.Y)
                         {
-                            if (i == food.X && j == food.Y)
+                            board[i, j] = "o";
+                        }
+                        else
+                        {
+                            bool isBodyPart = false;
+                            for (int count = 0; count < body.Length; count++)
                             {
-                                board[i, j] = "@";
+                                if (i == body[count].X && j == body[count].Y)
+                                {
+                                    board[i, j] = "+";
+                                    isBodyPart = true;
+                                    break;
+                                }
                             }
-                            else
+                            if (!isBodyPart)
                             {
-                                board[i, j] = " ";
+                                if (i == food.X && j == food.Y)
+                                {
+                                    board[i, j] = "@";
+                                }
+                                else
+                                {
+                                    board[i, j] = " ";
+                                }
                             }
                         }
                     }
                 }
             }
+            catch (Exception e)
+            {
+
+            }
+
         }
         // kiem tra va cham voi cac canh cua ban do
-        public static void MoveSnakeHead()
+        public  void MoveSnakeHead()
         {
-            switch (direction)
+            try
             {
-                case "Right":
-                    _head.Y += 1;
-                    if (_head.Y == col - 1)
-                    {
-                        /* _head.Y = 1; */
-                        SNAKE_DIE=true;
-                    }
-                    break;
-                case "Left":
-                    _head.Y -= 1;
-                    if (_head.Y == 0)
-                    {
-                       /*  _head.Y = col - 1; */
-                       SNAKE_DIE=true;
-                    }
-                    break;
-                case "Up":
-                    _head.X -= 1;
-                    if (_head.X == 0)
-                    {
-                      /*   _head.X = row - 1; */
-                      SNAKE_DIE=true;
-                    }
-                    break;
-                case "Down":
-                    _head.X += 1;
-                    if (_head.X == row - 1)
-                    {
-                       /*  _head.X = 1; */
-                       SNAKE_DIE=true;
-                    }
-                    break;
+                switch (direction)
+                {
+                    case "Right":
+                        _head.Y += 1;
+                        if (_head.Y == col - 1)
+                        {
+                            /* _head.Y = 1; */
+                            SNAKE_DIE = true;
+                        }
+                        break;
+                    case "Left":
+                        _head.Y -= 1;
+                        if (_head.Y == 0)
+                        {
+                            /*  _head.Y = col - 1; */
+                            SNAKE_DIE = true;
+                        }
+                        break;
+                    case "Up":
+                        _head.X -= 1;
+                        if (_head.X == 0)
+                        {
+                            /*   _head.X = row - 1; */
+                            SNAKE_DIE = true;
+                        }
+                        break;
+                    case "Down":
+                        _head.X += 1;
+                        if (_head.X == row - 1)
+                        {
+                            /*  _head.X = 1; */
+                            SNAKE_DIE = true;
+                        }
+                        break;
+                }
             }
+            catch (Exception e)
+            {
+
+            }
+
         }
         // doc vao phim len,xuong,trai,phai
-        public static void ListenKey()
+        public void ListenKey()
+
         {
-            while (true)
+            try
             {
-                ConsoleKeyInfo keyinfo = Console.ReadKey();
-                switch (keyinfo.Key)
+                while (true)
                 {
-                    case ConsoleKey.RightArrow:
-                        if (direction == "Up" || direction == "Down" || direction == "Left")
-                        {
-                            direction = "Right";
-                        }
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        if (direction == "Up" || direction == "Down" || direction == "Right")
-                        {
-                            direction = "Left";
-                        }
-                        break;
-                    case ConsoleKey.UpArrow:
-                        if (direction == "Left" || direction == "Right" || direction == "Down")
-                        {
-                            direction = "Up";
-                        }
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (direction == "Left" || direction == "Right" || direction == "Up")
-                        {
-                            direction = "Down";
-                        }
-                        break;
+                    ConsoleKeyInfo keyinfo = Console.ReadKey();
+                    switch (keyinfo.Key)
+                    {
+                        case ConsoleKey.RightArrow:
+                            if (direction == "Up" || direction == "Down" || direction == "Left")
+                            {
+                                direction = "Right";
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (direction == "Up" || direction == "Down" || direction == "Right")
+                            {
+                                direction = "Left";
+                            }
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (direction == "Left" || direction == "Right" || direction == "Down")
+                            {
+                                direction = "Up";
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (direction == "Left" || direction == "Right" || direction == "Up")
+                            {
+                                direction = "Down";
+                            }
+                            break;
+                    }
                 }
             }
+            catch (Exception e)
+            {
+
+            }
+
         }
         // hien thi ra ban do
-        public static void setUpBoard()
+        public void setUpBoard()
         {
-             
-            for (int i = 0; i < row; i++)
+            try
             {
-                for (int j = 0; j < col; j++)
+                for (int i = 0; i < row; i++)
                 {
-                    Console.Write(board[i, j]);
+                    for (int j = 0; j < col; j++)
+                    {
+                        Console.Write(board[i, j]);
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
+            catch (Exception e)
+            {
+
+            }
+
+
         }
         // hien thi thuc an
-        public static void PopUpfood()
+        public void PopUpfood()
+
         {
-            Random random = new Random();
-            int x = random.Next(1, row - 1);
-            int y = random.Next(1, col - 1);
-            if (x != _head.X && y != _head.Y)
+            try
             {
-                if (foodExist == false)
+                Random random = new Random();
+                int x = random.Next(1, row - 1);
+                int y = random.Next(1, col - 1);
+                if (x != _head.X && y != _head.Y)
                 {
-                    food.X = x;
-                    food.Y = y;
-                    foodExist = true;
+                    if (foodExist == false)
+                    {
+                        food.X = x;
+                        food.Y = y;
+                        foodExist = true;
+                    }
                 }
             }
+            catch (Exception e)
+            {
+
+            }
+
         }
         // tang size cua mang, khoi tao nut moi
-        public static void EatFood()
+        public void EatFood()
         {
-            if (_head.X == food.X && _head.Y == food.Y)
+            try
             {
-                score += 1;
-                Array.Resize(ref body, body.Length + 1);
-                body[body.Length - 1] = new Point(-1, -1);
-                speed -= 20;
-                foodExist = false;
+                if (_head.X == food.X && _head.Y == food.Y)
+                {
+                    score += 1;
+                    Array.Resize(ref body, body.Length + 1);
+                    body[body.Length - 1] = new Point(-1, -1);
+                    speed -= 20;
+                    foodExist = false;
+                }
             }
+            catch (Exception e)
+            {
+
+            }
+
         }
         // tang do dai than ran
-        public static void SpawnBody()
+        public void SpawnBody()
         {
-            for (int i = body.Length - 1; i > 0; i--)
+            try
             {
-                body[i].X = body[i - 1].X;
-                body[i].Y = body[i - 1].Y;
+                for (int i = body.Length - 1; i > 0; i--)
+                {
+                    body[i].X = body[i - 1].X;
+                    body[i].Y = body[i - 1].Y;
+                }
+                if (body.Length > 0)
+                {
+                    body[0].X = _head.X;
+                    body[0].Y = _head.Y;
+                }
             }
-            if (body.Length > 0)
+            catch (Exception e)
             {
-                body[0].X = _head.X;
-                body[0].Y = _head.Y;
+
             }
+
         }
         // hien thi diem
-        public static void ShowPoint()
+        public void ShowPoint()
         {
-            Console.WriteLine($"Score: {score}");
+            try
+            {
+                Console.WriteLine($"Score: {score}");
+                Console.WriteLine($"Best Score: {BestScore}");
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+        public static string getHighScore()
+
+        {
+            string highScore;
+            try
+            {
+                string sourceFilePath = @"/Users/loopo/OneDrive/Desktop/BT Codegym/ran_san_moi/save.csv";
+                highScore = File.ReadAllText(sourceFilePath);
+
+            }
+            catch (Exception e)
+            {
+                highScore = "1";
+            }
+            return highScore;
+        }
+        public static void saveNewHighScore(string highscore)
+        {
+            try
+            {
+                string sourceFilePath = @"/Users/loopo/OneDrive/Desktop/BT Codegym/ran_san_moi/save.csv";
+                File.WriteAllText(sourceFilePath, highscore);
+            }
+            catch (Exception e)
+            {
+
+            }
+
         }
     }
 }
